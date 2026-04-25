502173e9f3fc3eb880ee052ddf363473daf9e41e
OmarGarciaRamos <al238054@alumnos.uacj.mx>
2026-04-25 00:48:26 -0600

    Update script.js

--- script.js
+++ script.js
@@ -737,10 +737,9 @@ function updateEnemigos() {
 
     if (e.vida <= 0) {
             let suerte = Math.random(); 
-
-            if (suerte < 1/15) { 
-               
-                objetosSuelo.push({ x: e.x, y: e.y, tipo: "llave", width: 16, height: 16 });
+            if(suerte < 1/15){
+              objetosSuelo.push({x: e,x, y: e.y, tipo: "llave", width: 16, height: 16});
+            }
             } else if (suerte < 2/15) { 
                 objetosSuelo.push({ x: e.x, y: e.y, tipo: "corazon", width: 16, height: 16 });
             }
